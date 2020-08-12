672e48dd9451be80e980c6ea527444eb3a97659c
sfoutz0205 <68661461+sfoutz0205@users.noreply.github.com>
2020-08-11 20:29:04 -0600

    enemyNames for loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,15 +1,20 @@
+// Game States
+// "WIN" - Player robot has defeated all enemy robots
+//  *Fight all enemy robots
+//  *Defeat each enemy robot
+// "LOSE" - Player robot's health is zero or less
+
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-console.log(playerName, playerAttack, playerHealth);
-var enemyName = "Roborto";
-
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+//Fight 
+var fight = function(enemyName) {
     window.alert("Welcome to Robot Gladiators!");
 
 var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or ;'SKIP' to choose.");
@@ -60,4 +65,6 @@ if(playerHealth <= 0) {
 
 };
 
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
